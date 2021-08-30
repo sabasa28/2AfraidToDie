220778edbd4f96b2ed72675b7e3ca21b7114d8f5
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2021-08-30 20:31:27 -0300

    Changed button-door interaction

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,9 +2,32 @@
 
 public class Door : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] bool isOpen = false;
+
+    private void Start()
+    {
+        if (isOpen) animator.SetTrigger("Open");
+        else animator.SetTrigger("Close");
+    }
+    public void SwitchState()
+    {
+        if (isOpen) Close();
+        else Open();
+    }
+
     public void Open()
     {
+        if (isOpen) return;
         Debug.Log("door opened");
-        gameObject.SetActive(false);
+        animator.SetTrigger("Open");
+    } 
+
+    public void Close()
+    {
+        if (!isOpen) return;
+        Debug.Log("door closed");
+        animator.SetTrigger("Close");
     }
+
 }
--- Assets/Scripts/DoorButton.cs
+++ Assets/Scripts/DoorButton.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 public class DoorButton : MonoBehaviour
 {
     [SerializeField] Door door;
-
+    [SerializeField] bool canBePressed = true;
+    [SerializeField] bool onlyOneUse = true;
+    [SerializeField] Animator animator;
     static public event Action OnDoorOpen;
     static public event Action OnDoorClosed;
 
     public void OpenDoor()
     {
-        door.gameObject.SetActive(false);
-
+        door.Open();
+        canBePressed = false;
+        animator.SetTrigger("Press");
         OnDoorOpen?.Invoke();
     }
 
     public void CloseDoor()
     {
-        door.gameObject.SetActive(true);
+        door.Close();
+        canBePressed = false;
+        animator.SetTrigger("Press");
 
         OnDoorClosed?.Invoke();
     }
