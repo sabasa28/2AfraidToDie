bd07d27b2486713ef53ba2335962ea408af191b7
Iñaki Diez Galarza <inakidiezgalarza@gmail.com>
2021-11-12 21:12:12 -0300

    Erased not used assets, changed lens flare

---
+++ Assets/Scripts/Extra/FlareUpdater.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlareUpdater : MonoBehaviour
+{
+    public float size = 3.0f;
+
+    void Update()
+    {
+        float ratio = Mathf.Sqrt(Vector3.Distance(transform.position, Camera.main.transform.position));
+
+        GetComponent<LensFlare>().brightness = size / ratio;
+    }
+}
