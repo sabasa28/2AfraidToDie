1a67be51e24e95ce8376e6eaa06e90eb63d26dd4
LucioRuixo <54895404+LucioRuixo@users.noreply.github.com>
2021-11-16 11:16:03 -0300

    Prompt dialog interaction polished

--- Assets/Scripts/UI/Dialogs/PromptDialog.cs
+++ Assets/Scripts/UI/Dialogs/PromptDialog.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 
 public class PromptDialog : Dialog
 {
+    [Header("Prompt Dialog")]
     [SerializeField] TMP_InputField inputField = null;
 
     public TMP_InputField InputField { get { return inputField; } }
+
+    void Start() => inputField.Select();
+
+    public void CheckInputText(string text) => Buttons[DialogManager.ButtonType.Continue].Interactable = !string.IsNullOrEmpty(text);
 }
